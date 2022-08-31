2db9839f5e198551837557bba7cfceb295f026b7
kennie-create <kennieboulin1989@gmail.com>
2022-08-30 21:20:25 -0400

    Added state to welcome page, added buttons for redirect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <main class="full-height background-violet">
 
-    <h1>"I am in the right place at the right time, doing the right thing" - Louise Hay </h1>
-    </main>
-  );
+  const [welcomePage, setWelcomePage] = useState(true);
+
+  function dailyDosePage() {
+    setWelcomePage(false);
+  }
+
+  function returnToWelcomePage(){
+    setWelcomePage(true);
+  }
+
+  if (welcomePage) {
+    return (
+      <main class="full-height background-violet">
+        <h1> Your daily dose of gratitude awaits </h1>
+        <button onClick={dailyDosePage}> Reveal </button>
+      </main>
+    );
+  } else {
+    return (
+      <main class="full-height background-violet">
+        <h1>
+          "I am in the right place at the right time, doing the right thing" -
+          Louise Hay{" "}
+        </h1>
+        <button onClick={returnToWelcomePage}> Finish </button>
+      </main>
+    );
+  }
 }
 
 export default App;
